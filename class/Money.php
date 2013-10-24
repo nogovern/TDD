b361134d7a14bc14f88e7d1add29a37e5ba88818
Kwanghee Jang <nogovern@Kwanghee-Jang-ui-MacBook-Pro.local>
2013-10-24 15:06:54 +0900

    9-10장 추상클래스, 메서드 를 concrete class, method 로 변경 후 times() 에서 Money 인스턴스를 반환하도록 변경 하위 클래스들이 남아있는게 없으므로 제거해도 된다

--- class/Money.php
+++ class/Money.php
@@ -4,48 +4,50 @@
  * - value object pattern 사용
  */
 
-abstract class Money {
+class Money {
     protected   $amount;          // 하위 클래스 가시성 때문에 private -> protected 로 변경
     protected   $currency;
-
-    abstract function times($multiplier);
 
     public function __construct($amount, $currency) {
         $this->amount = $amount;
         $this->currency = $currency;
+
+        return $this;
     }
 
     // php 에서는 객체의 type casting 이 필요 없음
     public function equals($object) {
-        return ($this->amount == $object->amount && get_class($this) == get_class($object));
+        return ($this->amount == $object->amount && $this->currency() == $object->currency());
     }
 
     public function currency() {
         return $this->currency;
     }
 
+    public function times($multiplier) {
+        return new Money( $this->amount * $multiplier, $this->currency());
+    }
     /////////////////////
     // factory mothod
     /////////////////////
     public static function dollar($amount) {
-        return new Dollar($amount, "USD");
+        return new Money($amount, "USD");
     }
 
     public static function franc($amount) {
-        return new Franc($amount, "CHF");
+        return new Money($amount, "CHF");
     }
 }
 
+//
+// There is no need to exist sub classes.
+// therefore below classes will be removed...
 
 class Dollar extends Money {
 
     public function __construct($amount, $currency = "USD") {
         parent::__construct($amount, $currency);
         return $this;
-    }
-
-    public function times($multiplier = 1) {
-        return new Dollar($this->amount * $multiplier, $this->currency());
     }
     
 }
@@ -56,10 +58,6 @@ class Franc extends Money {
     public function __construct($amount, $currency = "CHF") {
         parent::__construct($amount, $currency);
         return $this;
-    }
-
-    public function times($multiplier = 1) {
-        return new Franc($this->amount * $multiplier, $this->currency());
     }
 
 }
